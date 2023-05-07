106307fb30a2927cd3fefa18e3aea5d63f174282
lihini <lswimalasooriya@gmail.com>
2023-05-07 04:10:04 -0700

    commit 2

---
+++ ios_cw/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_cw
+//
+//  Created by Lihini Wimalasooriya on 2023-05-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .red
+    }
+
+
+}
+
